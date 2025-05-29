46e71c46a39b8ef5e2d7fa8db49e8e637e50cb44
jafe2211 <jakob.fels@web.de>
2025-05-29 23:31:52 +0200

    added html mail option to mailHandler

--- src/util/mailHandler.ts
+++ src/util/mailHandler.ts
@@ -33,4 +33,33 @@ export class MailHandler {
             console.error(`Failed to send email to ${to}:`, error);
         }
     }
+
+        static async sendHtmlMail(to: string, subject: string, html: string): Promise<void> {
+        var transporter = nodemailer.createTransport({
+            host:  ConfigHandler.config.settings.mail.mailHost,
+            port:  ConfigHandler.config.settings.mail.mailPort,
+            secure: ConfigHandler.config.settings.mail.mailSecure, 
+            auth: {
+                user: ConfigHandler.config.settings.mail.mailUser,
+                pass: ConfigHandler.config.settings.mail.mailPass
+            }});
+        try {
+            await transporter.sendMail({
+                from:  "test123@test.se",
+                to: to,
+                subject: subject,
+                html: html
+            }, (error, info)=> {
+                if(error){
+                    log(`Error sending email to ${to}: ${error}`, "error");
+                    return;
+                }
+
+                log(`Email sent to ${to}: ${info.response}`, "info");
+            });
+
+        } catch (error) {
+            console.error(`Failed to send email to ${to}:`, error);
+        }
+    }
 }
